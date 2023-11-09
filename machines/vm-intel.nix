25663f7f5b91d898543540d870274793bd261c44
Matt Nodurfth <mnodurft@gmail.com>
2023-11-09 11:44:11 -0700

    Move to hardware file

--- machines/vm-intel.nix
+++ machines/vm-intel.nix
@@ -4,25 +4,4 @@
     ./vm-shared.nix
   ];
 
-  virtualisation.vmware.guest.enable = true;
-
-  nixpkgs.config.allowUnfree = true;
-
-  # Interface is this on Intel Fusion
-  networking.interfaces.ens33.useDHCP = true;
-
-  # Shared folder to host works on Intel
-  # https://github.com/mitchellh/nixos-config/issues/9; commenting out
-  #fileSystems."/host" = {
-  #  fsType = "fuse./run/current-system/sw/bin/vmhgfs-fuse";
-  #  device = ".host:/";
-  #  options = [
-  #    "umask=22"
-  #    "uid=1000"
-  #    "gid=1000"
-  #    "allow_other"
-  #    "auto_unmount"
-  #    "defaults"
-  #  ];
-  #};
 }
